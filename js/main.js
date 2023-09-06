df4172632753338788b65df51a172af5dbb75600
agl5702 <advasquez41@misena.edu.co>
2023-09-06 07:57:00 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function texto(){
+    document.getElementById('s3').innerHTML= '<h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, officia ut perspiciatis ex rem sapiente esse aliquid quia qui placeat reprehenderit animi. Explicabo, laudantium! Suscipit asperiores excepturi repellendus ipsam nemo?</h1>'
+}
+function f3(){
+    swal({
+        title: "Bien hecho!",
+        icon: "success",
+        button: "Salir",
+    });
+}
